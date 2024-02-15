9700ea5705720760b9fe382f0e63d3733f43735d
anca6 <526100@student.saxion.nl>
2024-02-15 16:09:54 +0100

    2-screen prototype done
    player click on the button and activates the ar camera

--- Project Innovation/Assets/Scenes/ANCA/CameraBehavior.cs
+++ Project Innovation/Assets/Scenes/ANCA/CameraBehavior.cs
@@ -4,11 +4,24 @@ using Vuforia;
 
 public class CameraBehavior : MonoBehaviour
 {
-    public GameObject arCamera;
+    public GameObject arCameraGameObj;
+    public Camera arCameraComponent;
 
+    public GameObject canvasGameObj;
 
+    //public Camera canvasCamera;
+
+    private void Start()
+    {
+        arCameraComponent.enabled = false;
+
+        canvasGameObj.SetActive(true);
+}
     public void ToggleCamera()
     {
-        arCamera.SetActive(!arCamera.activeSelf);
+        arCameraComponent.enabled = !arCameraComponent.enabled;
+
+        canvasGameObj.SetActive(!arCameraComponent.enabled);
+       // canvasCamera.gameObject.SetActive(arCameraComponent.enabled);
     }
 }
