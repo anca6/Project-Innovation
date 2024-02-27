db0a456e4cb10f9a2649bbb4763a0e28f1cc6e06
anca6 <526100@student.saxion.nl>
2024-02-27 23:07:01 +0100

    added game(scene) manager and game scenes

---
+++ Project Innovation/Assets/Scenes/ANCA/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void GoToNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ReloadCurrentScene()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        SceneManager.LoadScene(currentSceneName);
+    }
+
+    public void GoToThisScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+
+}
+  
