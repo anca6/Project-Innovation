bb4480cd9a26398caf64a7646135167a16437477
robert-oprea <538397@student.saxion.nl>
2024-03-07 15:12:50 +0100

    tweaked garden settings in the "garden" scene

---
+++ Project Innovation/Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QWY4RWlwei8vLy8vQUFBQm1YVkhscTJCK1VzVmh2c3MwaDRyTU9PSTQxVUlzVmRPbW5iclpPdmE4bVRMUHpuREZYRk04Z0xBM3I4MmFFU0NuSFhkUUtseDFvbExkeUpUVXpzWXlaQVFyREhURExlRmVERk4xOWF1RE9pWXhNU3BIT0ZGdjBIdVlBeVQ0NmU1VjlzRk9lOFFaeVlPSWtjTVE2OWpIVkY4Q3RoWUdkUU00NndxdVRWSEFNeGt3VlJjZEQ4VzNjMFVaelpta1BBREo0MmxVdlA4Y0hKZStmUEsxT3JZSWNuaWRJYVg4L1ZRZkVTTWZnaC94YkdZWkphbUdyWVNOU2pMdnBoUWR6c1E3ZjlNelVVWGg1bXRFejdJK1pLNHFnNnY3V3kzRGhxZFYwcHl1ZEtrd2d3VXJoMXhzT0VIbGhTeVZUWkN0NnJqenZHbWNFbkE4dWMyNlc3Y2xuNEZ4bUtHejJLVVdGTUFMRGhNUnVhV2VWTWM=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
