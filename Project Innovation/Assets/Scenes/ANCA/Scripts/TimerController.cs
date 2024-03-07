0ec46de586d9413381741cd5b140e915462833b9
anca6 <526100@student.saxion.nl>
2024-03-07 12:01:15 +0100

    flower timer and bug fixing

---
+++ Project Innovation/Assets/Scenes/ANCA/Scripts/TimerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+    private bool timerStarted = false;
+    public float duration = 30f;
+
+    private void Update()
+    {
+        if (timerStarted)
+        {
+            float elapsedTime = Time.time - startTime;
+            float remainingTime = Mathf.Max(0f, duration - elapsedTime);
+            int hours = (int)(remainingTime / 3600f);
+            int minutes = (int)(remainingTime / 60f);
+            int seconds = (int)(remainingTime % 60f);
+            timerText.text = string.Format("Timer: {0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+
+            if (remainingTime <= 0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+
+    }
+
+    public void StartTimer()
+    {
+        if (!timerStarted)
+        {
+            startTime = Time.time;
+            timerStarted = true;
+        }
+    }
+}
+
