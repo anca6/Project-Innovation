e51f071ae6814728333fac5065dbfe20280268b4
Rares Orza <rares.orza@gmail.com>
2024-03-07 11:18:33 +0100

    Groudn Texture and Sound Working

--- Project Innovation/Assets/Scenes/RARES/Scripts/AudioManager.cs
+++ Project Innovation/Assets/Scenes/RARES/Scripts/AudioManager.cs
@@ -1,30 +1,7 @@
 using UnityEngine.Audio;
-using UnityEngine;
 using System;
+using UnityEngine;
 using Unity.VisualScripting;
-
-[System.Serializable]
-public class Sound
-{
-
-    public string name;
-
-
-    public AudioClip clip;
-
-    [Range(0f, 1f)]
-    public float volume;
-    [Range(.1f, 3f)]
-    public float pitch;
-
-    public bool loop;
-
-    [HideInInspector]
-    public AudioSource source;
-
-}
-
-
 
 public class AudioManager : MonoBehaviour
 {
@@ -77,21 +54,5 @@ public class AudioManager : MonoBehaviour
             Debug.Log("Playing: " + name);
         }
         s.source.Play();                                            // FindObjectOfType<AudioManager>().Play("Sound_Name");
-    }
-
-    public void PlayWithPitch(string name, float pitch)
-    {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-        if (s == null)
-        {
-            Debug.LogWarning("Sound: " + name + " not found!");
-            return;
-        }
-        else
-        {
-            Debug.Log("Playing: " + name + " with pitch: " + pitch);
-        }
-        s.source.pitch = pitch;
-        s.source.Play();
     }
 }
---
+++ Project Innovation/Assets/Scenes/RARES/Scripts/ToggleSound.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleSound : MonoBehaviour
+{
+    public void ToggleClick()
+    {
+        FindObjectOfType<AudioManager>().Play("Click");
+    }
+}
